8a1545830cf21deb64c97e14fd1b4789ac6c3a6a
Matthew Chang <m_chang@mit.edu>
2015-11-09 22:15:46 -0500

    Parker rules

--- NN_m.py
+++ NN_m.py
@@ -45,29 +45,36 @@ def feedforward(X, Y, w1, w2):
 	z2 = w2 * o1 # k-length array
 	o2 = sigmoid(z2)
 	
-	dldo2 = np.matrix([-(Y[k,0] - o2[k,0]) for k in range(0,o2.shape[0])]).T
+	#dldo2 = np.matrix([-(Y[k,0] - o2[k,0]) for k in range(0,o2.shape[0])]).T
+	dldo2 = o2 - Y
 	
 	#dldo2 = np.matrix([(Y[k,0]+o2[k,0])/(o2[k,0]*(o2[k,0]-1)) for k in range(0,o2.shape[0])]).T
 	
 	dldz2 = np.multiply(dldo2,np.multiply(o2,1-o2))
 	dldw2 = np.zeros(w2.shape)
 	
-	for i in range(0,dldw2.shape[0]):
-		for j in range(0,dldw2.shape[1]):
-			dldw2[i,j] = o1[j,0]*dldz2[i,0]
+	#for i in range(0,dldw2.shape[0]):
+	#	for j in range(0,dldw2.shape[1]):
+        #           dldw2[i,j] = o1[j,0]*dldz2[i,0]
+
+	dldw2 = dldz2 * o1.T
 
 	dldo1 = np.zeros(o1.shape)
-	for i in range(0,o1.shape[0]):
-		for j in range(0,dldz2.shape[0]):
-			dldo1[i,0] += w2[j,i]*dldz2[j,0]
-			
+	#for i in range(0,o1.shape[0]):
+	#	for j in range(0,dldz2.shape[0]):
+	#		dldo1[i,0] += w2[j,i]*dldz2[j,0]
+
+	dldo1 = w2.T*dldz2
+	
 	dldz1 = np.multiply(dldo1,np.multiply(o1,1-o1))			
 		
 	dldw1 = np.zeros(w1.shape)
-	for i in range(0,dldw1.shape[0]):
-		for j in range(0,dldw1.shape[1]):
-			dldw1[i,j] = X[j,0]*dldz1[i,0]
-			
+	#for i in range(0,dldw1.shape[0]):
+#   		for j in range(0,dldw1.shape[1]):
+#			dldw1[i,j] = X[j,0]*dldz1[i,0]
+        #print w1.shape
+	dldw1 = np.delete(dldz1,-1,axis = 0) * X.T
+	#print dldw1.shape	
 	error = np.linalg.norm(Y-o2,2)
 
 	return (o2,dldw1,dldw2,error)
@@ -143,7 +150,7 @@ def error_rate(X,Y,w1,w2):
 			error += 1
 	return 1.0*error/(X.shape[0])
 
-hidden = 7	
+hidden = 2	
 w1 = np.ones((hidden,X.shape[1]+1))
 w2 = np.ones((possible_classes,hidden+1))
 w1,w2 = gradient_descent(X,Y,w1,w2,0.04,0)
